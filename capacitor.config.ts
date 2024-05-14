74409da5775409dc91a643aa63f91b42bec701b3
Emilio Robles <ejroblesl24@gmail.com>
2024-05-14 12:10:28 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.examenfinal',
+  appName: 'ph-examenfinal',
+  webDir: 'www'
+};
+
+export default config;
